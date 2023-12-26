fd43cf54e8916309a9a1c65fb34291770f3ef463
adan chavez <adanch22@gmail.com>
2023-12-26 13:36:54 -0600

    add component.spec

--- src/app/intermediate/Spy/medicos.component.spec.ts
+++ src/app/intermediate/Spy/medicos.component.spec.ts
@@ -1,37 +1,82 @@
-import { from, of } from 'rxjs';
+import { empty, from, Observable, throwError } from 'rxjs';
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
  
  
-describe('MedicosComponent', () => {
+describe('Medicos Component', () => {
  
     let componente: MedicosComponent;
-    const url:string = 'http'; 
-    let servicio: MedicosService;
+    const servicio = new MedicosService(null as any);
  
     beforeEach( () => {
-        const spy = jasmine.createSpyObj('HttpClient', { post: of({}), get: of({}) })
-        servicio = new MedicosService(spy);
         componente = new MedicosComponent(servicio);
     });
  
  
-    it('Init: Debe de cargar los médicos', () => {
+    it('Init: Debe cargar los medicos', () => {
+        const medicos = ['medico1', 'medico2', 'medico3'];
  
-        const medicos = ['medico1', 'medico2', 'medico3','medico4'];
- 
-        spyOn( servicio, 'getMedicos' ).and.callFake( () => {
-            return from( [ medicos ] );
-        });
- 
+        spyOn(servicio, 'getMedicos').and.callFake(()=>{
+            return from([medicos]);
+        })
  
         componente.ngOnInit();
  
-        expect( componente.medicos.length ).toBeGreaterThan(0);
- 
-   
+        expect(componente.medicos.length).toBeGreaterThan(0);
     });
  
+    it('Debe llamar al servidor para agregar un médico', () => {
+        const espia = spyOn(servicio, 'agregarMedico').and.callFake(()=> new Observable());
  
+        componente.agregarMedico();
+ 
+        expect(espia).toHaveBeenCalled();
+    });
+
+    it('Debe de agregar medico al arreglo de medicos', () => {
+        const medico = { id: 1, nombre: 'adan'};
+
+        spyOn(servicio, 'agregarMedico')
+            .and.returnValue(from([medico]));
+
+        componente.agregarMedico();
+        expect(componente.medicos.length).toBe(1);
+
+        expect(componente.medicos.indexOf(medico)).toBeGreaterThanOrEqual(0);
+    });
+
+
+    it('Si falla la adicion del medico, la porpiedad de mensajeError, debe ser igual al error', () => {
+        const myError = 'No se puede agregar el medico'; 
+
+        spyOn(servicio, 'agregarMedico').and
+            .returnValue(throwError(myError));
+    });
+
+
+    it('debe llamar al servidor para borrar un medico ', () => {
+
+        spyOn(window, 'confirm')
+            .and.returnValue(true);
+            
+        const spy = spyOn(servicio, 'borrarMedico')
+            .and.returnValue(empty());
+
+        componente.borrarMedico('1');
+
+        expect(spy).toHaveBeenCalledWith('1');
+    });
+
+    it('no debe llamar al servidor para borrar un medico ', () => {
+
+        spyOn(window, 'confirm')
+            .and.returnValue(false);
+            
+        const spy = spyOn(servicio, 'borrarMedico')
+            .and.returnValue(empty());
+
+        componente.borrarMedico('1');
+
+        expect(spy).not.toHaveBeenCalledWith('1');
+    });
 });
- 
